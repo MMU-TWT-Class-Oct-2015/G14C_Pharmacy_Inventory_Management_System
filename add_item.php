b5d53dc52bdc6e578a79ace3f4de9994b0ab7d6b
zulhilmi1812 <zulhilmi1812@gmail.com>
2016-02-12 02:30:33 +0800

    fix and add fuction
    fix name on input type
    add fuction to let page add item to database
    still can't connect to database

--- add_item.php
+++ add_item.php
@@ -27,15 +27,39 @@
   <input type="button" name="l_item" value="List Item">
   <input type="button" name="S_item" value="Search Item">
   <input type="button" name="logout" value="Logout" >
-  <br><br>
   <form   method="post" >
-  Item ID:  <input type="text" name="I_id" maxlength="25" size="35"><br>
-  Name: <input type="text" name="name_I" maxlength="25" size="35"><br>
-  Quantity: <input type="text" name="add_Q" maxlength="25" size="35"><br>
-  Price: <input type="text" name="add_Price" maxlength="25" size="35"><br>
-  Description: <input type="textarea" name="create_Des" maxlength="25" size="35"><br>
-  <input type="button" name="submit_i" value="Add" style="margin-right:70px;">
-  <input type="reset" name="reset_i" value="Clear" style="margin-right:70px;">
+  Item ID:  <input type="text" name="Item_ID" maxlength="25" size="35"><br>
+  Name: <input type="text" name="name" maxlength="25" size="35"><br>
+  Quantity: <input type="text" name="Quantity" maxlength="25" size="35"><br>
+  Price: <input type="text" name="Price" maxlength="25" size="35"><br>
+  Description: <input type="textarea" name="Des" maxlength="25" size="35"><br>
+  <input type="button" name="submit" value="Add" style="margin-right:70px;">
+  <input type="reset" name="reset" value="Clear" style="margin-right:70px;">
+
+  <?php
+ // check if the form has been submitted. If it has, start to process the form and save it to the database
+ if (isset($_POST['submit']))
+ {
+ // get form data, making sure it is valid
+ $Item_ID = mysql_real_escape_string(htmlspecialchars($_POST['Item_ID']));
+ $name = mysql_real_escape_string(htmlspecialchars($_POST['name']));
+ $Quantity = mysql_real_escape_string(htmlspecialchars($_POST['Quantity']));
+ $Price = mysql_real_escape_string(htmlspecialchars($_POST['Price']));
+ $Des = mysql_real_escape_string(htmlspecialchars($_POST['Des']));
+
+ // check to make sure both fields are entered
+ if ($Item_ID == '' || $name == ''|| $Quantity == '' || $Price == ''|| $Des == '' )
+
+ {
+ // save the data to the database
+ mysql_query("INSERT pharmacy_inventory SET Item_ID='$Item_ID', name='$name', Quantity='$Quantity', Price='$Price', Des=' $Des'")
+ or die(mysql_error());
+
+ // once saved, redirect back to the view page
+ header("Location: Detail_L_item.php");
+ }
+?>
+
 
 
 </body>
