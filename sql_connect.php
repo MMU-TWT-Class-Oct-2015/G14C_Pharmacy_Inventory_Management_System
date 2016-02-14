9e9d097b24839de4a946091973c7277d8596a79b
Turkiman <Turkiman>
2016-02-14 12:45:39 +0800

    updated user content

--- sql_connect.php
+++ sql_connect.php
@@ -1,4 +1,13 @@
 <?php
-mysql_connect("localhost","admin", "12345") or die("mysql connection is failure.");
-mysql_select_db("pinms") or die("Database does not exists.");
+
+$db_host= "127.0.0.1:8080/phpmyadmin/";
+$db_username= "root";
+$db_pass= "";
+$db_name= "pharmacy_inventory";
+
+ @mysql_connect("$db_host","$db_username","$db_pass") or die ("Could not connect to MySQL");
+ @mysql_select_db ("$db_name") or die ("No database");
+
+ //echo "Succesful connection";
+
 ?>
