5a4755190ef77a6e969e256c3fe91ab7137777a9
Abu Zaki Bin Abu Din <abuzaki.abudin@gmail.com>
2016-02-15 04:40:18 +0800

    display from db

---
+++ V_D_sale.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>View Daily Sales</title>
+    <link rel="stylesheet" type="text/css" href="stylingpage.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="script.js"></script>
+</head>
+
+<body>
+  <h3 class="skm">
+  Pharmacy Inventory Management System
+  </h3>
+  <div id='cssmenu'>
+    <ul>
+      <li><a href="index_admin.php"><span>Main Menu</span></a></li>
+      <li><a href="add_user.php"><span>Add User</span></a></li>
+      <li><a href="add_item.php"><span>Add Item</span></a></li>
+      <li class='active'><a href="V_D_sale.php"><span>Daily Sales</span></a></li>
+      <li><a href="V_M_sale.php"><span>Monthly Sales</span></a></li>
+      <li><a href="V_F_month.php"><span>Top Selling Item</span></a></li>
+      <li><a href="Update_Item.php"><span>Update Item</span></a></li>
+      <li><a href="delete.php"><span>Delete Item</span></a></li>
+      <li class='last'><a href="Home.html"><span>Log Out</span></a></li>
+    </ul>
+  </div>
+<div class="center">
+  <p>View Daily Sales</p>
+
+  <br><br>
+  <form  method="post" >
+  Date:  <input type="text" name="day_D" maxlength="2" size="2"> /
+  <input type="text" name="month_D" maxlength="2" size="2"> /
+  <input type="text" name="year_D" maxlength="4" size="4">
+  <input type="submit" name="submit_D" value="Search" style="margin-right:70px;">
+<?php
+error_reporting(E_ALL);
+session_start();
+include "conn.php";
+
+if (isset($_POST['submit_D']))
+{
+	// get form data, making sure it is valid
+
+	$day = $_POST['day_D'];
+	$month = $_POST['month_D'];
+	$year = $_POST['year_D'];
+
+	// save the data to the database
+	$query = "SELECT *
+  FROM `sales`
+  WHERE `day` = '$day' AND `month` = '$month' AND `year` = '$year'
+  ORDER BY `id_item`" ;
+ 
+	$result = mysqli_query($conn, $query);
+  if (mysqli_num_rows($result) > 0) {
+      echo "<table>
+  		<tr>
+    <th>Item ID</th>
+    <th>Item Sold</th>		
+    <th>Quantity Sold</th>
+    <th>Price</th> 		
+    <th>Day</th>
+    <th>Month</th>
+    <th>Year</th>
+  </tr>";
+      while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+      	
+      	echo "
+  
+  <tr>
+    <td>{$row['id_item']}</td>
+    <td>{$row['item_sold']}</td>		
+    <td>{$row['quantity_sold']}</td>
+    <td>{$row['price']}</td>
+    <td>{$row['day']}</td>
+    <td>{$row['month']}</td>
+    <td>{$row['year']}</td>
+  </tr>";
+      		
+      }
+      echo "</table>";
+    }
+      else {    echo "<br><br>Query didnt return any result";
+        }
+}
+ mysqli_close($conn)
+?>
+
+
+</form>
+</div>
+
+</body>
+</html>
